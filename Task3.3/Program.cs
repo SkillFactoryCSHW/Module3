1469c7aa649501baa788a537754a5514eb3a5da2
Bochanikita <bochanikita@yandex.ru>
2022-07-30 09:37:40 +0300

    Add information about me

--- Task3.3/Program.cs
+++ Task3.3/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string myName = "Nikita";
+byte myAge = 28;
+bool haveIPet = false;
+double myShoeSize = 45;
+
+Console.WriteLine("My name is {0}", myName);
+Console.WriteLine("My age {0}", myAge);
+Console.WriteLine("Do i have a pet? {}", haveIPet);
+Console.WriteLine("My shoe size is {}", myShoeSize);
